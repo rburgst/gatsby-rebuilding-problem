adac8ddf7452c2bc71c47ac1c942fd81011f1adc
Rainer Burgstaller <rburgst@gmail.com>
2020-05-11 14:59:55 +0200

    attempt to use @sanderploegsma solution

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,100 +4,92 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const { execute } = require("apollo-link")
+
+const { makePromise } = require("apollo-link")
+
 // You can delete this file if you're not using it
 const createPosts = require(`./gatsby/createPosts`)
 const { createRemoteFileNode } = require(`gatsby-source-filesystem`)
+const { createHttpLink } = require("apollo-link-http")
+const fetch = require("node-fetch")
+const gql = require("graphql-tag")
+
+const uri = process.env.GRAPHQL_ENDPOINT || "https://demo.wpgraphql.com/graphql"
+const headers = {}
 
 exports.createPages = async ({ actions, graphql }) => {
   await createPosts({ actions, graphql })
 }
 
-exports.createResolvers = ({
-  actions,
-  cache,
-  createNodeId,
-  createResolvers,
-  getNode,
-  store,
-  reporter,
-}) => {
-  const { createNode, touchNode } = actions
+exports.sourceNodes = async ({ actions, getCache, store, reporter }) => {
+  const { createNode } = actions
+  const link = createHttpLink({
+    uri,
+    fetch,
+    headers,
+  })
+  const query = gql`
+    query FetchImages($after: String) {
+      mediaItems(first: 100, after: $after) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        nodes {
+          sourceUrl
+        }
+      }
+    }
+  `
 
-  // Add all media libary images so they can be queried by
-  // childImageSharp
+  let hasNextPage
+  let after
+  do {
+    console.log("fetching images from ", uri, query, after)
+    const { data } = await makePromise(
+      execute(link, { query, variables: { after } })
+    )
+    console.log("done fetching images from ", uri, query, after)
+    await Promise.all(
+      data.mediaItems.nodes.map(({ sourceUrl }) => {
+        if (!sourceUrl || sourceUrl === "false") return Promise.resolve()
+        return createRemoteFileNode({
+          url: sourceUrl,
+          parentNodeId: null,
+          store,
+          getCache,
+          createNode,
+          createNodeId: () => sourceUrl,
+          httpHeaders: headers,
+          reporter,
+        })
+      })
+    )
+    hasNextPage = data.mediaItems.pageInfo.hasNextPage
+    after = data.mediaItems.pageInfo.endCursor
+  } while (hasNextPage)
+}
+
+exports.createResolvers = ({ createResolvers }) => {
   createResolvers({
     WPGraphQL_MediaItem: {
       mediaItemUrlSharp: {
-        type: `File`,
-        resolve(source, args, context, info) {
-          const sourceUrl = source.sourceUrl
-          if (sourceUrl) {
-            let fileNodeID
-            let fileNode
-            let sourceModified
-
-            // Set the file cacheID, get it (if it has already been set)
-            const mediaDataCacheKey = `wordpress-media-${source.mediaItemId}`
-            const cacheMediaData = cache.get(mediaDataCacheKey)
-
-            if (source.modified) {
-              sourceModified = source.modified
-            }
-
-            // If we have cached media data and it wasn't modified, reuse
-            // previously created file node to not try to redownload
-            if (cacheMediaData && sourceModified === cacheMediaData.modified) {
-              fileNode = getNode(cacheMediaData.fileNodeID)
-
-              // check if node still exists in cache
-              // it could be removed if image was made private
-              if (fileNode) {
-                fileNodeID = cacheMediaData.fileNodeID
-                // https://www.gatsbyjs.org/docs/node-creation/#freshstale-nodes
-                touchNode({
-                  nodeId: fileNodeID,
-                })
-              }
-            }
-
-            // If we don't have cached data, download the file
-            if (!fileNodeID) {
-              try {
-                // Get the filenode
-                console.log(`creating filenode for ${sourceUrl}`)
-
-                reporter.info(`creating filenode for ${sourceUrl}`)
-                fileNode = createRemoteFileNode({
-                  url: sourceUrl,
-                  store,
-                  cache,
-                  createNode,
-                  createNodeId,
-                  reporter,
-                })
-
-                if (fileNode) {
-                  fileNodeID = fileNode.id
-
-                  cache.set(mediaDataCacheKey, {
-                    fileNodeID,
-                    modified: sourceModified,
-                  })
-                }
-              } catch (e) {
-                // Ignore
-                console.log(e)
-                return null
-              }
-            }
-
-            if (fileNode) {
-              return fileNode
-            }
-          }
-          return null
-        },
+        type: "File",
+        resolve: (source, args, context) =>
+          context.nodeModel.getNodeById({ id: source.sourceUrl, type: "File" }),
       },
     },
   })
 }
+
+// exports.createResolvers = ({ createResolvers }) => {
+//   createResolvers({
+//     MediaItem: {
+//       localFile: {
+//         type: "File",
+//         resolve: (source, args, context) => context.nodeModel.getNodeById({ id: source.sourceUrl, type: "File" })
+//       }
+//     }
+//   })
+// }
